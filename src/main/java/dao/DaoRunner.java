4d794a194cb5e942f90de7b5615393b7983b61aa
Olegarh86 <malegik86@mail.ru>
2025-12-15 16:08:30 +0500

    docs: change gitignore

--- src/main/java/dao/DaoRunner.java
+++ src/main/java/dao/DaoRunner.java
@@ -5,6 +5,7 @@ import model.Currency;
 import model.ExchangeRate;
 
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.sql.Driver;
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,7 @@ public class DaoRunner {
     public static void main(String[] args) {
         CurrenciesFilter filter = new CurrenciesFilter("USD", "RUB");
         ExchangeRateDao exchangeRateDao = ExchangeRateDao.getInstance();
+        CurrencyDao currencyDao = CurrencyDao.getInstance();
         List<ExchangeRate> all = exchangeRateDao.findAll(filter);
         CurrencyDao instance = CurrencyDao.getInstance();
         for (ExchangeRate exchangeRate : all) {
@@ -20,8 +22,12 @@ public class DaoRunner {
             System.out.println(instance.findById(exchangeRate.getBaseCurrencyId()));
             System.out.println(instance.findById(exchangeRate.getTargetCurrencyId()));
         }
+        System.out.println(" ");
+        int CurrentCurrency = currencyDao.findCurrencyIdByCode("RUB");
+        Optional<Currency> optionalCurrency = currencyDao.findById(CurrentCurrency);
+        System.out.println(optionalCurrency);
 
-        exchangeRateDao.save(new ExchangeRate(instance.findCurrencyIdByCode(filter.base_currency_code()),
-                instance.findCurrencyIdByCode(filter.target_currency_code()), new BigDecimal("0.01")));
+//        exchangeRateDao.save(new ExchangeRate(instance.findCurrencyIdByCode(filter.base_currency_code()),
+//                instance.findCurrencyIdByCode(filter.target_currency_code()), new BigDecimal("0.01")));
     }
 }
